bd6a4431382e265e75f565e232a5237f61ad5619
website-krysta <website.krysta@gmail.com>
2023-12-09 13:29:27 +0530

    fixed issues

--- src/components/sections/home/Aboutcompany.js
+++ src/components/sections/home/Aboutcompany.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import ReactPlayer from 'react-player';
-
+import { Player } from 'video-react';
 class TimelineSection extends Component {
 
     
@@ -27,48 +27,40 @@ class TimelineSection extends Component {
 
           
         return (
-            <section id="timeline " style={{...backgroundStyles}}>
-                {/* <h5 className="custom-primary text-center white-bg  mb-5  mx-auto" style={{textAlign:"center"}}>Founder Message</h5> */}
-                <h2 className="title text-center mt-2">Tenali Double Horse</h2> 
-                <p className="text-center" style={{width:'75%', margin:"auto"}}> At Teanli Double Horse  Our Journey Begins in 2005 with avision to bring excellence  and trust to every Household in india Guided by our commitment to quality and customer Satisfication , we have since Become a trusted name that resonates with millions </p> 
-        {/* <h2 className="title text-center">A Timeline of Growth, Innovation, and Impact</h2> */}
-        
-            
-                <div className="fluid-container video-conatiner mt-3">
-                       
-                   
-                    
-                    <div className="row ourstory-section text-center"  >
-                      <div className='col-lg-8 col-12' >
-                       
-                      
-                      {/* <ReactPlayer
-            url={"./assets/video/tdh-our-story.mp4"}
-            controls poster={process.env.PUBLIC_URL + "/assets/img/misc/great-place-to-work.png"}
-            width="100%"
-            height="100%"
-          /> */}
-           <video className='shadow p-1 mb-5 bg-white rounded image-radius' width="90%" height="80%" controls poster={process.env.PUBLIC_URL + "/assets/img/misc/our-story-poster.png"}>
-                          <source src="./assets/video/tdh-chairmen-video.mp4" type="video/mp4" />
-                          {/* <source src={process.env.PUBLIC_URL + "./assets/video/tdh-chairmen-video.mp4"} type="video/mp4" /> */}
-                      </video>
-                
-                <a
-                  
-                 
-                  className="btn-custom btn-sm shadow-none suma mb-5 "
-                  target="_blank"
-                 
-                >
-                  READ OUR STORY
-                </a>    
-                      </div>
-                    </div>
-                </div>
+          <section id="timeline " style={{ ...backgroundStyles }}>
+            {/* <h5 className="custom-primary text-center white-bg  mb-5  mx-auto" style={{textAlign:"center"}}>Founder Message</h5> */}
+            <h2 className="title text-center mt-2">Tenali Double Horse</h2>
+            <p className="text-center" style={{ width: '75%', margin: "auto" }}> At Teanli Double Horse  Our Journey Begins in 2005 with avision to bring excellence  and trust to every Household in india Guided by our commitment to quality and customer Satisfication , we have since Become a trusted name that resonates with millions </p>
+            {/* <h2 className="title text-center">A Timeline of Growth, Innovation, and Impact</h2> */}
+            <div className="fluid-container video-conatiner mt-3">
+              <div className="row ourstory-section text-center"  >
+                <div className='col-lg-8 col-12' >
 
-               
-                    
-            </section>
+                  {/* <ReactPlayer
+                    url={"./assets/video/tdh-chairmen-video.mp4"}
+                    controls
+                    poster={process.env.PUBLIC_URL + "/assets/img/misc/our-story-poster.png"}
+                    width="90%"
+                    height="80%"
+                  /> */}
+                
+                  <video className='shadow p-1 mb-5 bg-white rounded image-radius' width="90%" height="80%" controls poster={process.env.PUBLIC_URL + "/assets/img/misc/our-story-poster.png"}>
+                          <source src=".././assets/video/tdh-chairmen-video.mp4" type="video/mp4" /> 
+                      </video>
+                  {/* <source src={process.env.PUBLIC_URL + "./assets/video/tdh-chairmen-video.mp4"} type="video/mp4" /> */}
+
+                  <a
+                    className="btn-custom btn-sm shadow-none suma mb-5 "
+                    target="_blank"
+                  > READ OUR STORY
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+
+          </section>
         );
     }
 }

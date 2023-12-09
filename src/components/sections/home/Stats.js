76a46fa204fb299d72ad4f28e82c29d8f13471f4
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2023-12-09 13:22:14 +0530

    icons

--- src/components/sections/home/Stats.js
+++ src/components/sections/home/Stats.js
@@ -1,61 +1,56 @@
 import React, { Component } from 'react';
 import WOW from 'wow.js';
 import 'animate.css';
-import  box from '../../../assets/img/bg/box 2.svg'
-
-
+import boxImage from '../../../assets/img/bg/box 2.svg'; // Import the image
 
 const successpost = [
     {
-        icon: "fas fa-medal",
+        icon: 'assets/img/banner/medal.png',
         count: 18,
         title: "Years of Experience",
-        delay:"0.4s"
+        delay: "0.4s"
     },
     {
-        icon:  "fas fa-globe",
+        icon: 'assets/img/banner/globe.png',
         count: 12,
         title: "Total countries",
-        delay:"0.6s"
+        delay: "0.6s"
     },
-   
     {
-        icon:  "fas fa-map",
+        icon: 'assets/img/banner/india.png',
         count: 18,
         title: "States",
-        delay:"0.8s"
+        delay: "0.8s"
     },
-    
     {
-        icon: "fas fa-box-open",
+        icon: 'assets/img/banner/box.png',
         count: 10,
         title: "Products",
-        delay:"0.9s"
+        delay: "0.9s"
     }
-    
 ]
 
-class stats extends Component {
+class Stats extends Component {
     componentDidMount() {
-        new WOW().init(); 
+        new WOW().init();
     }
     render() {
         return (
-            <div className="section section-padding  bg-cover bg-center  " style={{ backgroundImage: "url(" + process.env.PUBLIC_URL + "/assets/img/categories/stats-tdh.png)" }} >
+            <div className="section section-padding  bg-cover bg-center " style={{ backgroundImage: "url(" + process.env.PUBLIC_URL + "/assets/img/categories/stats-tdh.png)" }} >
                 <div className="container" >
                     <div className="section-title-wrap section-header text-center">
                         <h2 className="title text-white">Empowering Progress</h2>
                         <p className="subtitle text-white">
-                        Explore the statistics that define Tenali Double Horse's journey. From years of excellence to a growing customer base, discover the metrics that highlight our commitment to quality and growth.
+                            Explore the statistics that define Tenali Double Horse's journey. From years of excellence to a growing customer base, discover the metrics that highlight our commitment to quality and growth.
                         </p>
                     </div>
-                    <div className="row ">
+                    <div className="row " >
                         {successpost.map((item, i) => (
                             <div key={i} className="col-lg-3 col-md-6 col-sm-6 wow animate__animated animate__fadeInUp" data-wow-delay={item.delay}>
-                                <div className="ct-infographic-item card pt-3 pb-3 with-border-bottom border-0 shadow rounded-3">
-                                    <i className={item.icon} style={{color:'#DE7E76'}} />
-                                    <h4 className='text-dark' style={{fontWeight:'800'}}>{new Intl.NumberFormat().format(item.count)}</h4>
-                                    <p className='text-dark' style={{fontWeight:'800'}}>{item.title}</p>
+                                <div className="ct-infographic-item card pt-3 pb-3 with-border-bottom border-0 shadow rounded-3" style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+                                    <img src={ item.icon} alt={item.title} style={{ width: '100px', height: '100px' }} /> {/* Use the image here */}
+                                    <h4 className='text-dark' style={{ fontWeight: '800' }}>{new Intl.NumberFormat().format(item.count)}</h4>
+                                    <p className='text-dark' style={{ fontWeight: '800' }}>{item.title}</p>
                                 </div>
                             </div>
                         ))}
@@ -66,4 +61,4 @@ class stats extends Component {
     }
 }
 
-export default stats;
+export default Stats;

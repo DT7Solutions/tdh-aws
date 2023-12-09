5c05eb240c08ce72da32c1c36c26c2ae50877cfc
website-krysta <website.krysta@gmail.com>
2023-12-09 16:18:30 +0530

    change video

--- src/components/sections/home/Aboutcompany.js
+++ src/components/sections/home/Aboutcompany.js
@@ -36,16 +36,16 @@ class TimelineSection extends Component {
               <div className="row ourstory-section text-center"  >
                 <div className='col-lg-8 col-12 mx-auto' >
 
-                  {/* <ReactPlayer
+                  <ReactPlayer
                     url={"./assets/video/tdh-chairmen-video.mp4"}
                     controls
                     poster={process.env.PUBLIC_URL + "/assets/img/misc/our-story-poster.png"}
                     width="100%"
                     height="60%"
-                  /> */}
+                  />
 
-                  
-                 <iframe width="700" height="415" src="https://www.youtube.com/embed/kmjjov75uSI?si=BxxlkuhWZ2jppSJ-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+                 {/* <iframe width="700" height="415" src="https://www.youtube.com/embed/kmjjov75uSI?si=BxxlkuhWZ2jppSJ-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
                 
                   {/* <video className='shadow p-1 mb-5 bg-white rounded image-radius' width="100%" height="60%" controls poster={process.env.PUBLIC_URL + "/assets/img/misc/our-story-poster.png"}>
                           <source src="/assets/video/tdh-chairmen-video.mp4" type="video/mp4" /> 
